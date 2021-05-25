3a7ad070b2daad444e03a0f757668dc7579f6edc
Aakash Kaushik <kaushikaakash7539@gmail.com>
2021-05-25 20:30:32 +0530

    grad_cam integration in app.py

--- app.py
+++ app.py
@@ -22,15 +22,17 @@ class EffNet(nn.Module):
         return x
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+use_cuda = True if device=="cuda" else False
 model = nn.DataParallel(EffNet(image_size))
 model.load_state_dict(torch.load("./models/efnet-b5-best.pth",
     map_location=device))
 model = model.to(device)
 model.eval()
-print(model)
 
-grad_cam = GradCam(model=model, blob_name = '_blocks',
-    target_layer_names=['38'], use_cuda=(True if device=="cuda" else False))
+grad_cam = GradCam(model=model.module, blob_name = '_blocks',
+    target_layer_names=['38'], use_cuda=use_cuda)
+gb_model = GuidedBackpropReLUModel(model=model.module,
+    activation_layer_name="SiLU", use_cuda=use_cuda)
 
 tfms = transforms.Compose([transforms.Resize(image_size),
                            transforms.Grayscale(num_output_channels=1),
@@ -41,6 +43,14 @@ def process(img, tfms):
   img = tfms(img)
   img_tf = torch.unsqueeze(img, 0).to(device)
   return img_tf
+
+def grad_process(res, img, img_tf, gb_model):
+  target_idx = torch.argmax(res)
+  img = np.array(img) / 255
+  img_tf = img_tf.requires_grad_(True)
+  mask_disc = grad_cam(img_tf, target_idx)
+  show_cams(img, mask_disc)
+  show_gbs(img_tf, gb_model, target_idx, mask_disc)
 
 st.title("Covid Identifier")
 st.write("""Upload your CT scans for prediction you can also upload multiple
@@ -57,11 +67,14 @@ if len(img_list) != 0:
   for prog, st_img in enumerate(img_list):
     img = Image.open(st_img)
     if combine:
-      
+      img_tf = process(img, tfms)
       res += model(img_tf)
+      grad_process(res, img, img_tf, gb_model)
       bar.progress(int(prog * 100/len(img_list)) + int(100/len(img_list)))
     else:
+      img_tf = process(img, tfms)
       res = model(img_tf)
+      grad_process(res, img, img_tf, gb_model)
       bar.progress(int(prog * 100/len(img_list)) + int(100/len(img_list)))
       st.text(st_img.name + ": " + labels[torch.argmax(res)])
   if combine:
