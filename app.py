31b8eebeeff94c2862a9f65b314ac9ac089e297e
Aakash Kaushik <kaushikaakash7539@gmail.com>
2021-05-26 20:59:49 +0530

    working covid app

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import torch 
-from grad_cam import *
 import torch.nn as nn
 from PIL import Image
 import streamlit as st
@@ -22,35 +21,21 @@ class EffNet(nn.Module):
         return x
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-use_cuda = True if device=="cuda" else False
 model = nn.DataParallel(EffNet(image_size))
 model.load_state_dict(torch.load("./models/efnet-b5-best.pth",
     map_location=device))
 model = model.to(device)
 model.eval()
 
-grad_cam = GradCam(model=model.module, blob_name = '_blocks',
-    target_layer_names=['38'], use_cuda=use_cuda)
-gb_model = GuidedBackpropReLUModel(model=model.module,
-    activation_layer_name="SiLU", use_cuda=use_cuda)
-
 tfms = transforms.Compose([transforms.Resize(image_size),
                            transforms.Grayscale(num_output_channels=1),
                            transforms.ToTensor(),
                            transforms.Normalize(img_mean, img_std)])
 
-def process(img, tfms):
+def process(img, tfms, model):
   img = tfms(img)
-  img_tf = torch.unsqueeze(img, 0).to(device)
-  return img_tf
-
-def grad_process(res, img, img_tf, gb_model):
-  target_idx = torch.argmax(res)
-  img = np.array(img) / 255
-  img_tf = img_tf.requires_grad_(True)
-  mask_disc = grad_cam(img_tf, target_idx)
-  show_cams(img, mask_disc)
-  show_gbs(img_tf, gb_model, target_idx, mask_disc)
+  img = torch.unsqueeze(img, 0).to(device)
+  return model(img)
 
 st.title("Covid Identifier")
 st.write("""Upload your CT scans for prediction you can also upload multiple
@@ -67,14 +52,10 @@ if len(img_list) != 0:
   for prog, st_img in enumerate(img_list):
     img = Image.open(st_img)
     if combine:
-      img_tf = process(img, tfms)
-      res += model(img_tf)
-      grad_process(res, img, img_tf, gb_model)
+      res += process(img, tfms, model)
       bar.progress(int(prog * 100/len(img_list)) + int(100/len(img_list)))
     else:
-      img_tf = process(img, tfms)
-      res = model(img_tf)
-      grad_process(res, img, img_tf, gb_model)
+      res += process(img, tfms, model)
       bar.progress(int(prog * 100/len(img_list)) + int(100/len(img_list)))
       st.text(st_img.name + ": " + labels[torch.argmax(res)])
   if combine:
