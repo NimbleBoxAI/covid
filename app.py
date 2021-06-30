f37f2e2558f06ba294b0cbdbcc83a27fe7ceaa60
niladridutt <niladrishekhardutt@gmail.com>
2021-06-30 13:54:17 +0300

    fix bugs

--- app.py
+++ app.py
@@ -11,15 +11,13 @@ labels = ['Covid', 'Normal', 'Pneumonia']
 img_mean, img_std = [0.459], [0.347]
 image_size = (456, 456)
 target_index = 0
-
-device = torch.device("cpu")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = EfficientNet.from_name('efficientnet-b5', in_channels=1, image_size = image_size, num_classes=3)
 model.load_state_dict(torch.load("./models/model.pth", map_location=device))
 model = model.to(device)
 model.eval()
-use_cuda = True if device == "cuda" else False 
+use_cuda = True if device == torch.device("cuda") else False
 grad_cam = GradCam(model=model, blob_name = '_blocks', target_layer_names=['25'], use_cuda=use_cuda)
-gb_model = GuidedBackpropReLUModel(model=model, activation_layer_name = 'MemoryEfficientSwish', use_cuda=use_cuda)
 
 tfms = transforms.Compose([transforms.Resize(image_size),
                            transforms.Grayscale(num_output_channels=1),
@@ -37,22 +35,19 @@ st.write("""Covid detection network trained on axial views of CT scan images.
 
 st_img = st.file_uploader("Upload files here")
 if st_img != None:
-  res = 0
-  bar = st.progress(0)
   img = Image.open(st_img).resize((456, 456))
   img.save('img.jpg')
   img_tf = process(tfms, img.convert('RGB'))
-  res+= model(img_tf)
+  res = model(img_tf)
   target_index = torch.argmax(res[-1])
   img = np.array(img) / 255
   img_tf = img_tf.requires_grad_(True)
   mask_dic = grad_cam(img_tf, target_index)
   show_cams(img, mask_dic)
+  gb_model = GuidedBackpropReLUModel(model=model, activation_layer_name = 'MemoryEfficientSwish', use_cuda=True)
   show_gbs(img_tf, gb_model, target_index, mask_dic)
-  bar.progress(int(prog * 100/len(img_list)) + int(100/len(img_list)))
   st.image(img)
   st.image('cam25.jpg')    
-  res = res/len(img_list)
   st.text("Predicted Class: " + labels[torch.argmax(res)])
 else:
   st.text("Please Upload an image")
