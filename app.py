1a1eeeda0992bf3d816681d3f11cb25a2f6060ee
Aakash Kaushik <kaushikaakash7539@gmail.com>
2021-06-30 16:06:43 +0530

    options for target device

--- app.py
+++ app.py
@@ -12,12 +12,14 @@ img_mean, img_std = [0.459], [0.347]
 image_size = (456, 456)
 target_index = 0
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device("cpu")
 model = EfficientNet.from_name('efficientnet-b5', in_channels=1, image_size = image_size, num_classes=3)
 model.load_state_dict(torch.load("./models/model.pth", map_location=device))
 model = model.to(device)
 model.eval()
-grad_cam = GradCam(model=model, blob_name = '_blocks', target_layer_names=['25'], use_cuda=True)
+use_cuda = True if device == "cuda" else False 
+grad_cam = GradCam(model=model, blob_name = '_blocks', target_layer_names=['25'], use_cuda=use_cuda)
+gb_model = GuidedBackpropReLUModel(model=model, activation_layer_name = 'MemoryEfficientSwish', use_cuda=use_cuda)
 
 tfms = transforms.Compose([transforms.Resize(image_size),
                            transforms.Grayscale(num_output_channels=1),
@@ -29,33 +31,27 @@ def process(tfms, img):
   img = torch.unsqueeze(img, 0).to(device)
   return img
 
-st.title("Covid Identifier")
-st.write("""Upload your CT scans for prediction you can also upload multiple
-            CT scans to predict multiple results or combine them to improve
-            results.""")
-st.write("Use the below checkbox for that selection before uploading images")
+st.title("Covid Detector")
+st.write("""Covid detection network trained on axial views of CT scan images.
+            Disclaimer : Only use axial views when predicting.""")
 
-combine = st.checkbox("Combine images for the result")
-img_list = st.file_uploader("Upload files here", accept_multiple_files=True)
-
-if len(img_list) != 0:
+st_img = st.file_uploader("Upload files here")
+if st_img != None:
   res = 0
   bar = st.progress(0)
-  for prog, st_img in enumerate(img_list):
-    img = Image.open(st_img).resize((456, 456))
-    img.save('img.jpg')
-    img_tf = process(tfms, img.convert('RGB'))
-    res+= model(img_tf)
-    target_index = torch.argmax(res[-1])
-    img = np.array(img) / 255
-    img_tf = img_tf.requires_grad_(True)
-    mask_dic = grad_cam(img_tf, target_index)
-    show_cams(img, mask_dic)
-    gb_model = GuidedBackpropReLUModel(model=model, activation_layer_name = 'MemoryEfficientSwish', use_cuda=True)
-    show_gbs(img_tf, gb_model, target_index, mask_dic)
-    bar.progress(int(prog * 100/len(img_list)) + int(100/len(img_list)))
-    st.image(img)
-    st.image('cam25.jpg')    
+  img = Image.open(st_img).resize((456, 456))
+  img.save('img.jpg')
+  img_tf = process(tfms, img.convert('RGB'))
+  res+= model(img_tf)
+  target_index = torch.argmax(res[-1])
+  img = np.array(img) / 255
+  img_tf = img_tf.requires_grad_(True)
+  mask_dic = grad_cam(img_tf, target_index)
+  show_cams(img, mask_dic)
+  show_gbs(img_tf, gb_model, target_index, mask_dic)
+  bar.progress(int(prog * 100/len(img_list)) + int(100/len(img_list)))
+  st.image(img)
+  st.image('cam25.jpg')    
   res = res/len(img_list)
   st.text("Predicted Class: " + labels[torch.argmax(res)])
 else:
